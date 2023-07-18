a960fed738d3d2afcb9c5b987e425fe4bdf17743
carlosklg <139086058+carlosklg@users.noreply.github.com>
2023-07-17 23:29:02 -0300

    aula 3

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -8,20 +8,16 @@ from dino_runner.utils.constants import (
     FPS,
 )
 from dino_runner.components.dinosaur import Dinosaur
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
 
 class Game:
     def __init__(self):
-        pygame.init()
-        pygame.display.set_caption(TITLE)
-        pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        self.playing = False
-        self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
+
 
     def run(self):
         self.__init__()
@@ -36,16 +32,33 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.playing = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.player.update_jump(True)
+                elif event.key == pygame.K_DOWN:
+                    self.player.update_duck(True)
+                elif event.key == pygame.K_SPACE:
+                    self.player.update_duck(True)
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    self.player.update_jump(False)
+                elif event.key == pygame.K_DOWN:
+                    self.player.update_duck(False)
+                elif event.key == pygame.K_SPACE:
+                    self.player.update_duck(False)
 
     def update(self):
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))  # "#FFFFFF"
         self.draw_background()
         self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
@@ -57,3 +70,6 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+
+
