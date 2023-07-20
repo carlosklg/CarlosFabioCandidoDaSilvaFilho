381b2d2ce9df70e239434d9a75a51d26ea9c1084
carlos fabio <carlosffbio18@gmail.com>
2023-07-20 04:55:31 -0300

    ATIVIDADE 5

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,4 +1,5 @@
 import pygame
+
 from dino_runner.utils.constants import (
     BG,
     ICON,
@@ -6,12 +7,13 @@ from dino_runner.utils.constants import (
     SCREEN_WIDTH,
     TITLE,
     FPS,
+    DEFAULT_TYPE,
 )
-
+from dino_runner.utils.text_utils import draw_message_component
 from dino_runner.components.dinosaur import Dinosaur
-from dino_runner.components.obstacles.obstacles_manager import ObstacleManager
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 
-FONT_STYLE = "freesansbold.ttf"
 
 class Game:
     def __init__(self):
@@ -25,11 +27,11 @@ class Game:
         self.game_speed = 20
         self.score = 0
         self.death_count = 0
-        self.reset_score = 0
         self.x_pos_bg = 0
         self.y_pos_bg = 380
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.power_up_manager = PowerUpManager()
 
     def execute(self):
         self.running = True
@@ -37,16 +39,19 @@ class Game:
             if not self.playing:
                 self.show_menu()
         pygame.display.quit()
-        pygame.quit()        
+        pygame.quit()
 
     def run(self):
+        # Game loop: events - update - draw
         self.playing = True
+        self.game_speed = 20
+        self.score = 0
         self.obstacle_manager.reset_obstacles()
+        self.power_up_manager.reset_power_ups()
         while self.playing:
             self.events()
             self.update()
             self.draw()
-        
 
     def events(self):
         for event in pygame.event.get():
@@ -58,6 +63,7 @@ class Game:
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self)
+        self.power_up_manager.update(self)
         self.update_score()
 
     def update_score(self):
@@ -65,18 +71,15 @@ class Game:
         if self.score % 100 == 0:
             self.game_speed += 5
 
-    def reset_score(self):
-        self.score = 0
-        self.game_speed = 20        
-    
-
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))  # "#FFFFFF"
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
         self.draw_score()
+        self.draw_power_up_time()
         pygame.display.update()
         pygame.display.flip()
 
@@ -90,11 +93,26 @@ class Game:
         self.x_pos_bg -= self.game_speed
 
     def draw_score(self):
-        font = pygame.font.Font(FONT_STYLE, 22)
-        text = font.render(f"Score: {self.score}", True, (0, 0, 0))
-        text_rect = text.get_rect()
-        text_rect.center = (1000, 50)
-        self.screen.blit(text, text_rect)
+        draw_message_component(
+            f"Score: {self.score}", self.screen, pos_x_center=1000, pos_y_center=50
+        )
+
+    def draw_power_up_time(self):
+        if self.player.has_power_up:
+            time_to_show = round(
+                (self.player.power_up_time - pygame.time.get_ticks()) / 1000, 1
+            )
+            if time_to_show >= 0:
+                draw_message_component(
+                    f"{self.player.type.capitalize()} enabled for {time_to_show} seconds",
+                    self.screen,
+                    font_size=18,
+                    pos_x_center=500,
+                    pos_y_center=40,
+                )
+            else:
+                self.player.has_power_up = False
+                self.player.type = DEFAULT_TYPE
 
     def show_menu(self):
         self.screen.fill((255, 255, 255))
@@ -102,14 +120,25 @@ class Game:
         half_screen_width = SCREEN_WIDTH // 2
 
         if self.death_count == 0:
-            font = pygame.font.Font(FONT_STYLE, 22)
-            text = font.render("Press any key to start", True, (0, 0, 0))
-            text_rect = text.get_rect()
-            text_rect.center = (half_screen_width, half_screen_height)
-            self.screen.blit(text, text_rect)
-        else: #tela de restart 
-            self.screen.blit(ICON, (half_screen_width - 20, half_screen_height - 140))
-       
+            draw_message_component("Press any key to start", self.screen)
+        else:
+            draw_message_component(
+                "Press any key to restart",
+                self.screen,
+                pos_y_center=half_screen_height + 140,
+            )
+            draw_message_component(
+                f"Your Score: {self.score}",
+                self.screen,
+                pos_y_center=half_screen_height - 150,
+            )
+            draw_message_component(
+                f"Death count: {self.death_count}",
+                self.screen,
+                pos_y_center=half_screen_height - 100,
+            )
+            self.screen.blit(ICON, (half_screen_width - 40, half_screen_height - 40))
+
         pygame.display.update()
         self.handle_events_on_menu()
 
@@ -120,5 +149,3 @@ class Game:
                 self.running = False
             elif event.type == pygame.KEYDOWN:
                 self.run()
-                
-
--- dino_runner/components/obstacles/obstacles_manager.py
+++ dino_runner/components/obstacles/obstacles_manager.py
@@ -4,10 +4,10 @@ import random
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.bird import Bird
 
+
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
-        
 
     def update(self, game):
         obstacle_type = [
@@ -16,16 +16,18 @@ class ObstacleManager:
         ]
 
         if len(self.obstacles) == 0:
-            self.obstacles.append(obstacle_type[random.randint(0,1)])
-            
-            
+            self.obstacles.append(obstacle_type[random.randint(0, 1)])
+
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(500)
-                game.playing = False
-                game.death_count += 1
-                break
+                if not game.player.has_power_up:
+                    pygame.time.delay(500)
+                    game.playing = False
+                    game.death_count += 1
+                    break
+                else:
+                    self.obstacles.remove(obstacle)
 
     def draw(self, screen):
         for obstacle in self.obstacles:
